00206b0a57734a9d340e1543c55c350cf5436f79
kyledobash <kyledobash@yahoo.com>
2021-04-05 10:39:15 -0700

    Completed DescendingOrder challenge. Still needs Refactor.

---
+++ CSharp_dotNet_Practice/CodeWarsKata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_dotNet_Practice
+{
+    public class CodeWarsKata
+    {
+        // take in any non-negative integer as an argument and return it with its digits in descending order
+        // in other words, rearrange the digits to create the highest possible number
+        //public int DescendingOrder(int num)
+        //{
+        //    string mynum = num.ToString();
+        //    var digits = mynum.Select(x => Convert.ToInt32(x.ToString())).ToList();
+        //    digits.Sort();
+        //    digits.Reverse();
+        //    string stringResult = "";
+        //    foreach (int digit in digits)
+        //    {
+        //        digit.ToString();
+        //        stringResult += digit;
+        //    }
+        //    int result = Convert.ToInt32(stringResult);
+        //    return result;
+        //}
+
+
+    }
+}
---
+++ CSharp_dotNet_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_dotNet_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeWarsKata test = new CodeWarsKata();
+            Console.WriteLine(test.DescendingOrder(16544));
+            Console.WriteLine(test.DescendingOrder(98547));
+            Console.WriteLine(test.DescendingOrder(1100000));
+            Console.WriteLine(test.DescendingOrder(0000011));
+        }
+    }
+}
