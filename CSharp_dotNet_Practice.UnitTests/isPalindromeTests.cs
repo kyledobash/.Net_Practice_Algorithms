fa4c0f07fc6fd4c0e5713adf6c8a870c9ebf920e
kyledobash <kyledobash@yahoo.com>
2021-05-03 17:19:01 -0700

    added inputEmptyString Palindrome test

---
+++ CSharp_dotNet_Practice.UnitTests/isPalindromeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharp_dotNet_Practice.UnitTests
+{
+    [TestClass]
+    public class isPalinDromeTests
+    {
+        [TestMethod]
+        public void isPalindrome_InputEmptyString_ReturnsTrue()
+        {
+            //arange
+            CodeWarsKata test = new CodeWarsKata();
+            string emptyString = "";
+
+            bool expected = true;
+            bool actual;
+
+            //act
+            actual = test.isPalindrome(emptyString);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
